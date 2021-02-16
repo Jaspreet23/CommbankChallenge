a3b864dfff25e080cbcde4b9dde8ef2311642b31
JasWoolieX <jbamrah@woolworths.com.au>
2021-02-17 01:54:39 +1100

    changes to Base class

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -48,42 +48,11 @@ public class base {
 	public static WebDriver driver;
 	public static Properties prop;
 	public static String AUTO_ENV;
-	public static String API_YN = "No";
-	public static String IRESS_YN = "No";
-	public static String newBusinessCaseId;
-	public static String NBC_Number;
-	public static String insuredID;
-	public static String insuredLifeId;
-	public static String quoteScenarioID;
-	public static String uwmeEnquiryID;
-	public static String ClientName;
-	public static String ClientLastName;
-	public static String portal_Username;
-	public static Map<String, String> default_Map = new HashMap<String, String>();
 	public static Map<String, String> data_Map = new HashMap<String, String>();
 	public static String testType = "";
 	public static String downloadFilepath;
-	public static String coverType = "";
-	public static String onlyCover = "";
-	public static String CSP_YN = "N";
-	public static String allCovers = "";
-	public static String ownership = "Personal";
 	public static boolean createReport = false;
 	public static Map<String, Integer> testResultRecorder = new HashMap<String, Integer>();
-	public static boolean Life = false;
-	public static boolean deletedCover = false;
-	public static int IRESS = 1;
-	public static WebDriver driver2;
-	public static String shoppingCart_YN = "No";
-	public static String policyNumber;
-	public static Integer policyID;
-	public static String policyNumber2;
-	public static Integer policyID2;
-	public static boolean IRESS_Pricing = false;
-	public static boolean UME_Scenario = false;
-	public static boolean UME_Scenario_Last = false;
-	public static String pdf_File_Name;
-	public static String zip_Folder_Name;
 
 	@AfterMethod
 	public void tearDownMethod(ITestResult result) throws IOException {
@@ -101,41 +70,6 @@ public class base {
 				System.getProperty("user.dir") + "/src/main/java/resources/data.properties");
 		prop.load(fis);
 		String browserName = prop.getProperty("browser");
-		// System.out.println(browserName);
-		//FileInputStream fis3 = new FileInputStream(
-				//System.getProperty("user.dir") + "\\src\\main\\java\\resources\\API.xlsx");
-
-		//XSSFWorkbook workbook = new XSSFWorkbook(fis3);
-		// DataFormatter objDefaultFormat=new DataFormatter();
-		// FormulaEvaluator objFormaulaEvaluator=new XSSFFormulaEvaluator(XSSFWorkbook)
-		// workbook;
-
-		//Sheet sheet1 = workbook.getSheetAt(0);
-		//int lastRow1 = sheet1.getLastRowNum();
-
-		//XSSFRow row = (XSSFRow) sheet1.getRow(0);
-
-		/*for (int i = 1; i <= lastRow1; i++) {
-
-			Row row1 = sheet1.getRow(i);
-
-			// 2nd Cell as Value
-
-			Cell valueCell1 = row1.getCell(2);
-
-			DataFormatter dataFormatter = new DataFormatter();
-			// objectFormulaEvaluator.evaluate(valueCell1);
-
-			// 1st Cell as Key
-			Cell keyCell1 = row1.getCell(1);
-
-			String key1 = keyCell1.getStringCellValue().trim();
-			// String value1=valueCell1.getStringCellValue().trim();
-			String value1 = dataFormatter.formatCellValue(valueCell1);
-			default_Map.put(key1, value1);
-
-		}*/
-
 		if (browserName.equals("chrome")) {
 			// execute in chrome driver
 			System.setProperty("webdriver.chrome.driver",
@@ -196,48 +130,12 @@ public class base {
 
 	}
 
-	@SuppressWarnings("deprecation")
-	public void DataInitialize(String filepath, String sheetName, int rowNumber) throws IOException {
-
-		prop = new Properties();
-		FileInputStream fileDetails = new FileInputStream(filepath);
-		System.out.println("filepath is  " + filepath);
-		System.out.println("Sheet name is " + sheetName);
-		@SuppressWarnings("resource")
-		XSSFWorkbook workbook = new XSSFWorkbook(fileDetails);
-		Sheet sheet = workbook.getSheet(sheetName);
-		XSSFRow HeaderRow = (XSSFRow) sheet.getRow(0);
-		XSSFRow currentRow = (XSSFRow) sheet.getRow(rowNumber);
-
-		// HashMap<String,Integer> currentHash = new HashMap<String,Integer>();
-		for (int j = 1; j < currentRow.getPhysicalNumberOfCells(); j++) {
-			XSSFCell currentCell = currentRow.getCell(j);
-			data_Map.put(HeaderRow.getCell(j).getStringCellValue(), currentCell.getStringCellValue());
-		}
-	}
-
 	public static void loadPropertiesFile() throws IOException {
 		prop = new Properties();
 		FileInputStream fis = new FileInputStream(
 				System.getProperty("user.dir") + "\\src\\main\\java\\resources\\data.properties");
 		prop.load(fis);
 
-	}
-
-	public static String readPropertiesFile(String readProperty) throws IOException {
-		loadPropertiesFile();
-//		prop= new Properties();
-//		FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\data.properties");
-//		prop.load(fis);
-		return prop.getProperty(readProperty);
-	}
-
-	public void getScreenshot(String result) throws IOException {
-		String timeStamp;
-		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-		FileUtils.copyFile(src, new File(System.getProperty("user.dir") + "//TestResults//Screenshots//" + result + "_"
-				+ timeStamp + "_" + "screenshot.png"));
 	}
 
 	public static void getScreenshot2() throws IOException {
@@ -250,8 +148,5 @@ public class base {
 				System.getProperty("user.dir") + "/TestResults/Screenshots/_" + timeStamp + "_" + "FullScreen.png"));
 	}
 
-	public static String generateStringFromResource(String path) throws IOException {
-		return new String(Files.readAllBytes(Paths.get(path)));
-	}
 
 }
